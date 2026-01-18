85689ecb187880b681bce4785e491a9d361c7856
ovlegend <ov.legend@gmail.com>
2026-01-18 03:00:25 -0500

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,7 +1,9 @@
 // Global site config
-window.OV_CONFIG = window.OV_CONFIG || {};
-window.OV_CONFIG.rlol = {
-  teamsCsv: "https://docs.google.com/spreadsheets/d/e/2PACX-1vQCQnxfwBylnd5H8jHc_g9Gtv7wyhzelCLixlK3-Bi_Uw0pVJga8MPtgYf5740Csm7hbfLTJhHGdWzh/pub?gid=0&single=true&output=csv",
-  scheduleCsv: "https://docs.google.com/spreadsheets/d/e/2PACX-1vQCQnxfwBylnd5H8jHc_g9Gtv7wyhzelCLixlK3-Bi_Uw0pVJga8MPtgYf5740Csm7hbfLTJhHGdWzh/pub?gid=907396704&single=true&output=csv",
-  standingsCsv: "https://docs.google.com/spreadsheets/d/e/2PACX-1vQCQnxfwBylnd5H8jHc_g9Gtv7wyhzelCLixlK3-Bi_Uw0pVJga8MPtgYf5740Csm7hbfLTJhHGdWzh/pub?gid=1039937861&single=true&output=csv"
+window.OV_CONFIG = {
+  rlol: {
+    teamsCsv: "https://docs.google.com/spreadsheets/d/e/2PACX-1vQCQnxfwBylnd5H8jHc_g9Gtv7wyhzelCLixlK3-Bi_Uw0pVJga8MPtgYf5740Csm7hbfLTJhHGdWzh/pub?gid=0&single=true&output=csv",
+    scheduleCsv: "https://docs.google.com/spreadsheets/d/e/2PACX-1vQCQnxfwBylnd5H8jHc_g9Gtv7wyhzelCLixlK3-Bi_Uw0pVJga8MPtgYf5740Csm7hbfLTJhHGdWzh/pub?gid=907396704&single=true&output=csv",
+    standingsCsv: "https://docs.google.com/spreadsheets/d/e/2PACX-1vQCQnxfwBylnd5H8jHc_g9Gtv7wyhzelCLixlK3-Bi_Uw0pVJga8MPtgYf5740Csm7hbfLTJhHGdWzh/pub?gid=128420471&single=true&output=csv",
+    standingsViewCsv: "https://docs.google.com/spreadsheets/d/e/2PACX-1vQCQnxfwBylnd5H8jHc_g9Gtv7wyhzelCLixlK3-Bi_Uw0pVJga8MPtgYf5740Csm7hbfLTJhHGdWzh/pub?gid=1039937861&single=true&output=csv"
+  }
 };
