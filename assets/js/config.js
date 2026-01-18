926bb55929cbc58caad7898c4d5c0ae6e579f320
ovlegend <ov.legend@gmail.com>
2026-01-18 01:15:52 -0500

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,8 +1,9 @@
 // Global site config
 window.OV_CONFIG = {
   rlol: {
-    teamsCsv: "",
-    standingsCsv: "",
-    scheduleCsv: ""
+    teamsCsv: "https://docs.google.com/spreadsheets/d/e/2PACX-1vQCQnxfwBylnd5H8jHc_g9Gtv7wyhzelCLixlK3-Bi_Uw0pVJga8MPtgYf5740Csm7hbfLTJhHGdWzh/pub?gid=0&single=true&output=csv
+",
+    standingsCsv: "https://docs.google.com/spreadsheets/d/e/2PACX-1vQCQnxfwBylnd5H8jHc_g9Gtv7wyhzelCLixlK3-Bi_Uw0pVJga8MPtgYf5740Csm7hbfLTJhHGdWzh/pub?gid=128420471&single=true&output=csv",
+    scheduleCsv: "https://docs.google.com/spreadsheets/d/e/2PACX-1vQCQnxfwBylnd5H8jHc_g9Gtv7wyhzelCLixlK3-Bi_Uw0pVJga8MPtgYf5740Csm7hbfLTJhHGdWzh/pub?gid=907396704&single=true&output=csv"
   }
 };
