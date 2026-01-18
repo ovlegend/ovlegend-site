878f5a5dd3fdaed33ff8661449d6ea542d2d9bdd
ovlegend <ov.legend@gmail.com>
2026-01-18 15:43:35 -0500

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -3,8 +3,8 @@ window.OV_CONFIG = {
   rlol: {
     teamsCsv: "https://docs.google.com/spreadsheets/d/e/2PACX-1vQCQnxfwBylnd5H8jHc_g9Gtv7wyhzelCLixlK3-Bi_Uw0pVJga8MPtgYf5740Csm7hbfLTJhHGdWzh/pub?gid=0&single=true&output=csv",
     scheduleCsv: "https://docs.google.com/spreadsheets/d/e/2PACX-1vQCQnxfwBylnd5H8jHc_g9Gtv7wyhzelCLixlK3-Bi_Uw0pVJga8MPtgYf5740Csm7hbfLTJhHGdWzh/pub?gid=907396704&single=true&output=csv",
-    standingsCsv: "https://docs.google.com/spreadsheets/d/e/2PACX-1vQCQnxfwBylnd5H8jHc_g9Gtv7wyhzelCLixlK3-Bi_Uw0pVJga8MPtgYf5740Csm7hbfLTJhHGdWzh/pub?gid=1284204718&single=true&output=csv",
-    standingsViewCsv: "https://docs.google.com/spreadsheets/d/e/2PACX-1vQCQnxfwBylnd5H8jHc_g9Gtv7wyhzelCLixlK3-Bi_Uw0pVJga8MPtgYf5740Csm7hbfLTJhHGdWzh/pub?gid=1039937861&single=true&output=csv",
+    standingsCsv: "https://docs.google.com/spreadsheets/d/1x3Aj1DdPOiskwZ7YGWwVIP_h3wmGM75dK6Nr_ZcS-nQ/gviz/tq?tqx=out:csv&gid=128420471",
+    standingsViewCsv: "https://docs.google.com/spreadsheets/d/1x3Aj1DdPOiskwZ7YGWwVIP_h3wmGM75dK6Nr_ZcS-nQ/gviz/tq?tqx=out:csv&gid=1039937861",
     playerGameStatsCsv: "https://docs.google.com/spreadsheets/d/e/2PACX-1vQCQnxfwBylnd5H8jHc_g9Gtv7wyhzelCLixlK3-Bi_Uw0pVJga8MPtgYf5740Csm7hbfLTJhHGdWzh/pub?gid=1283136814&single=true&output=csv",
     playerSeasonStatsCsv: "https://docs.google.com/spreadsheets/d/e/2PACX-1vQCQnxfwBylnd5H8jHc_g9Gtv7wyhzelCLixlK3-Bi_Uw0pVJga8MPtgYf5740Csm7hbfLTJhHGdWzh/pub?gid=939885276&single=true&output=csv"
   }
