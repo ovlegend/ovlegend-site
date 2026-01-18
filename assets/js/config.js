50e4245833eff0a94d76a702dac703f9b17655da
ovlegend <ov.legend@gmail.com>
2026-01-18 14:22:39 -0500

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -5,5 +5,7 @@ window.OV_CONFIG = {
     scheduleCsv: "https://docs.google.com/spreadsheets/d/e/2PACX-1vQCQnxfwBylnd5H8jHc_g9Gtv7wyhzelCLixlK3-Bi_Uw0pVJga8MPtgYf5740Csm7hbfLTJhHGdWzh/pub?gid=907396704&single=true&output=csv",
     standingsCsv: "https://docs.google.com/spreadsheets/d/e/2PACX-1vQCQnxfwBylnd5H8jHc_g9Gtv7wyhzelCLixlK3-Bi_Uw0pVJga8MPtgYf5740Csm7hbfLTJhHGdWzh/pub?gid=128420471&single=true&output=csv",
     standingsViewCsv: "https://docs.google.com/spreadsheets/d/e/2PACX-1vQCQnxfwBylnd5H8jHc_g9Gtv7wyhzelCLixlK3-Bi_Uw0pVJga8MPtgYf5740Csm7hbfLTJhHGdWzh/pub?gid=1039937861&single=true&output=csv"
+    playerGameStatsCsv: "https://docs.google.com/spreadsheets/d/e/2PACX-...&gid=1283136814&single=true&output=csv",
+    playerSeasonStatsCsv: "https://docs.google.com/spreadsheets/d/e/2PACX-...&gid=939885276&single=true&output=csv",
   }
 };
