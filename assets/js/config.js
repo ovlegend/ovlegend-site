7f6da4b22579e98f16b354010067b32445e2bac8
ovlegend <ov.legend@gmail.com>
2026-01-18 01:22:36 -0500

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,8 +1,7 @@
 // Global site config
 window.OV_CONFIG = {
   rlol: {
-    teamsCsv: "https://docs.google.com/spreadsheets/d/e/2PACX-1vQCQnxfwBylnd5H8jHc_g9Gtv7wyhzelCLixlK3-Bi_Uw0pVJga8MPtgYf5740Csm7hbfLTJhHGdWzh/pub?gid=0&single=true&output=csv
-",
+    teamsCsv: "https://docs.google.com/spreadsheets/d/e/2PACX-1vQCQnxfwBylnd5H8jHc_g9Gtv7wyhzelCLixlK3-Bi_Uw0pVJga8MPtgYf5740Csm7hbfLTJhHGdWzh/pub?gid=0&single=true&output=csv",
     standingsCsv: "https://docs.google.com/spreadsheets/d/e/2PACX-1vQCQnxfwBylnd5H8jHc_g9Gtv7wyhzelCLixlK3-Bi_Uw0pVJga8MPtgYf5740Csm7hbfLTJhHGdWzh/pub?gid=128420471&single=true&output=csv",
     scheduleCsv: "https://docs.google.com/spreadsheets/d/e/2PACX-1vQCQnxfwBylnd5H8jHc_g9Gtv7wyhzelCLixlK3-Bi_Uw0pVJga8MPtgYf5740Csm7hbfLTJhHGdWzh/pub?gid=907396704&single=true&output=csv"
   }
