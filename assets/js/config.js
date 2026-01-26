cae8d0f127b434ef31d8b4072791c3a4598ee337
ovlegend <ov.legend@gmail.com>
2026-01-26 15:53:07 -0500

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,11 +1,26 @@
-// Global site config
-window.OV_CONFIG = {
-  rlol: {
-    teamsCsv: "https://docs.google.com/spreadsheets/d/e/2PACX-1vQCQnxfwBylnd5H8jHc_g9Gtv7wyhzelCLixlK3-Bi_Uw0pVJga8MPtgYf5740Csm7hbfLTJhHGdWzh/pub?gid=0&single=true&output=csv",
-    scheduleCsv: "https://docs.google.com/spreadsheets/d/e/2PACX-1vQCQnxfwBylnd5H8jHc_g9Gtv7wyhzelCLixlK3-Bi_Uw0pVJga8MPtgYf5740Csm7hbfLTJhHGdWzh/pub?gid=907396704&single=true&output=csv",
-    standingsCsv: "https://docs.google.com/spreadsheets/d/1x3Aj1DdPOiskwZ7YGWwVIP_h3wmGM75dK6Nr_ZcS-nQ/gviz/tq?tqx=out:csv&gid=128420471",
-    standingsViewCsv: "https://docs.google.com/spreadsheets/d/1x3Aj1DdPOiskwZ7YGWwVIP_h3wmGM75dK6Nr_ZcS-nQ/gviz/tq?tqx=out:csv&gid=1039937861",
-    playerGameStatsCsv: "https://docs.google.com/spreadsheets/d/e/2PACX-1vQCQnxfwBylnd5H8jHc_g9Gtv7wyhzelCLixlK3-Bi_Uw0pVJga8MPtgYf5740Csm7hbfLTJhHGdWzh/pub?gid=1283136814&single=true&output=csv",
-    playerSeasonStatsCsv: "https://docs.google.com/spreadsheets/d/e/2PACX-1vQCQnxfwBylnd5H8jHc_g9Gtv7wyhzelCLixlK3-Bi_Uw0pVJga8MPtgYf5740Csm7hbfLTJhHGdWzh/pub?gid=939885276&single=true&output=csv"
-  }
+// /assets/js/config.js
+// Global site config (safe to re-run)
+
+window.OV_CONFIG = window.OV_CONFIG || {};
+
+OV_CONFIG.rlol = {
+
+  teamsCsv:
+    "https://docs.google.com/spreadsheets/d/e/2PACX-1vQCQnxfwBylnd5H8jHc_g9Gtv7wyhzelCLixlK3-Bi_Uw0pVJga8MPtgYf5740Csm7hbfLTJhHGdWzh/pub?gid=0&single=true&output=csv",
+
+  scheduleCsv:
+    "https://docs.google.com/spreadsheets/d/e/2PACX-1vQCnxfwBylnd5H8jHc_g9Gtv7wyhzelCLixlK3-Bi_Uw0pVJga8MPtgYf5740Csm7hbfLTJhHGdWzh/pub?gid=907396704&single=true&output=csv",
+
+  standingsCsv:
+    "https://docs.google.com/spreadsheets/d/e/2PACX-1vQCnxfwBylnd5H8jHc_g9Gtv7wyhzelCLixlK3-Bi_Uw0pVJga8MPtgYf5740Csm7hbfLTJhHGdWzh/pub?gid=128420471&single=true&output=csv",
+
+  standingsViewCsv:
+    "https://docs.google.com/spreadsheets/d/1x3AjIDdP0iskwZ7YGWMWIP_h3wmGM75dK6Nr_ZcS-n0/gviz/tq?tqx=out:csv&gid=1039937861",
+
+  playerGameStatsCsv:
+    "https://docs.google.com/spreadsheets/d/e/2PACX-1vQCnxfwBylnd5H8jHc_g9Gtv7wyhzelCLixlK3-Bi_Uw0pVJga8MPtgYf5740Csm7hbfLTJhHGdWzh/pub?gid=1283136814&single=true&output=csv",
+
+  playerSeasonStatsCsv:
+    "https://docs.google.com/spreadsheets/d/e/2PACX-1vQCnxfwBylnd5H8jHc_g9Gtv7wyhzelCLixlK3-Bi_Uw0pVJga8MPtgYf5740Csm7hbfLTJhHGdWzh/pub?gid=939885276&single=true&output=csv"
+
 };
