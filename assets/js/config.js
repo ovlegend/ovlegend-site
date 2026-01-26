d51fd2655f0caed65578b4d8e235437915e5e480
ovlegend <ov.legend@gmail.com>
2026-01-26 15:56:17 -0500

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -9,7 +9,7 @@ OV_CONFIG.rlol = {
     "https://docs.google.com/spreadsheets/d/e/2PACX-1vQCQnxfwBylnd5H8jHc_g9Gtv7wyhzelCLixlK3-Bi_Uw0pVJga8MPtgYf5740Csm7hbfLTJhHGdWzh/pub?gid=0&single=true&output=csv",
 
   scheduleCsv:
-    "https://docs.google.com/spreadsheets/d/e/2PACX-1vQCnxfwBylnd5H8jHc_g9Gtv7wyhzelCLixlK3-Bi_Uw0pVJga8MPtgYf5740Csm7hbfLTJhHGdWzh/pub?gid=907396704&single=true&output=csv",
+  "https://docs.google.com/spreadsheets/d/e/2PACX-1vQCQnxfwBylnd5H8jHc_g9Gtv7wyhzelCLixlK3-Bi_Uw0pVJga8MPtgYf5740Csm7hbfLTJhHGdWzh/pub?gid=907396704&single=true&output=csv",
 
   standingsCsv:
     "https://docs.google.com/spreadsheets/d/e/2PACX-1vQCnxfwBylnd5H8jHc_g9Gtv7wyhzelCLixlK3-Bi_Uw0pVJga8MPtgYf5740Csm7hbfLTJhHGdWzh/pub?gid=128420471&single=true&output=csv",
